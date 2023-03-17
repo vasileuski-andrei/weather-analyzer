4c1326a45b2997a5ce6802d71d8531ad2168ff28
Andrei Vasileuski <vasileuski.andrei@gmail.com>
2023-03-17 09:25:12 +0300

    add migration framework - liquibase; create db-script

---
+++ src/main/resources/db/changelog/db.changelog-1.0.sql
@@ -0,0 +1,19 @@
+--liquibase formatted sql
+
+--changeset andreiv:1
+CREATE TABLE IF NOT EXISTS weather_info(
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    region VARCHAR(50),
+    country VARCHAR(50),
+    timezone_id VARCHAR(50),
+    localtime_epoch VARCHAR(50),
+    local_time TIMESTAMP,
+    temp_c DOUBLE PRECISION,
+    temp_f DOUBLE PRECISION,
+    wind_mph DOUBLE PRECISION,
+    pressure_mb DOUBLE PRECISION,
+    pressure_in DOUBLE PRECISION,
+    humidity DOUBLE PRECISION,
+    cloud DOUBLE PRECISION
+);
