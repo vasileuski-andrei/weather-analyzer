3c757bdd5a0e65dda9d674317073c3807337af15
Andrei Vasileuski <vasileuski.andrei@gmail.com>
2023-04-13 14:21:53 +0300

    add controller test

--- src/test/java/com/senla/weatheranalyzer/integration/controller/WeatherControllerTest.java
+++ src/test/java/com/senla/weatheranalyzer/integration/controller/WeatherControllerTest.java
@@ -5,9 +5,11 @@ import com.senla.weatheranalyzer.integration.annotation.IT;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @IT
@@ -16,11 +18,28 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class WeatherControllerTest extends TestBase {
 
     private final MockMvc mockMvc;
+    public static final String URL = "/api/v1/weather";
 
     @Test
     void getCurrentWeatherInfoTest() throws Exception {
-        mockMvc.perform(get("/api/v1/weather"))
-                .andExpect(status().is2xxSuccessful());
+
+        mockMvc.perform(get(URL))
+                .andExpect(status().isOk());
+
+    }
+
+    @Test
+    public void getAverageWeatherInfoBetweenTwoDatesTest() throws Exception {
+
+        String date = "{ \"from\": \"18-03-2023\", \"to\": \"19-03-2023\" }";
+
+        mockMvc.perform(post(URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(date)
+                        .characterEncoding("utf-8"))
+                .andExpect(status().isOk())
+                .andReturn();
+
     }
 
 }
